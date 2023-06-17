668b30c3948b995e969014314e82c07a83ca30ba
Clara Borlu <claraborlu24@gmail.com>
2023-06-17 14:19:39 +0000

    Add: LoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 
-class CustomAuthenticationForm(AuthenticationForm):
+class LoginForm(forms.Form):
     email = forms.EmailField(widget=forms.TextInput(attrs={'autofocus': True}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
