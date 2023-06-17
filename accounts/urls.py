18d148bd2bdefac12d3fc8cb72b65deabd4796b2
Clara Borlu <claraborlu24@gmail.com>
2023-06-17 14:23:10 +0000

    Add: password-reset and logout urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path(route='home/', view=views.home, name='home'),
     path(route='signup/', view=views.signup, name='signup'),
-    path(route='account-activation-sent/', view=views.account_activation_sent, name='account_activation_sent'),
     path(route='activate/<uidb64>/<token>/', view=views.activate, name='activate'),
     path(route='login/', view=views.login_view, name='login'),
-    path(route='logout/', view=LogoutView.as_view(), name='logout'),
+    path(route='logout/', view=views.logout_view, name='logout'),
 ]
