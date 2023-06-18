5183c0b90be2a34149fa3f827dadd3435e8ebad1
Clara Borlu <claraborlu24@gmail.com>
2023-06-18 15:58:22 +0000

    Register File model

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import File
 
-# Register your models here.
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description','downloads', 'emails_sent', 'uploaded_at',)
+    list_filter = ('uploaded_at', 'downloads', 'emails_sent')
+    search_fields = ('title', 'description')
+    ordering = ('-uploaded_at',)
+
+admin.site.register(File, FileAdmin)
