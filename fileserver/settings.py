abe3a83dca2debf7182da4e0c7656cec064d1411
Clara Borlu <claraborlu24@gmail.com>
2023-06-19 12:04:51 +0000

    Change: Email and password

--- fileserver/settings.py
+++ fileserver/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kwabena4140@gmail.com'
-EMAIL_HOST_PASSWORD = 'aihefcptcvjzyyfv'
+EMAIL_HOST_USER = 'claraborlu24@gmail.com'
+EMAIL_HOST_PASSWORD = 'dwytchmooytfvcey'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
